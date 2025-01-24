719426f1c293c2affe3d26c7f0d38a877e62b6e6
ldkuba <nawrocki97@gmail.com>
2025-01-24 20:51:41 +0100

    Add test scene and character controller. Update gitattributes

---
+++ scripts/character.gd
@@ -0,0 +1,51 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+var mouse_input: Vector2 = Vector2.ZERO
+@export var sensitivity: Vector2 = Vector2(0.1, 0.1)
+
+@export var camera: Camera3D
+
+func _init() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		pass
+	elif event is InputEventMouseMotion:
+		mouse_input = event.relative
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Handle the mouse input.
+	rotate_y(-mouse_input.x * sensitivity.x * delta)
+	camera.rotate_x(-mouse_input.y * sensitivity.y * delta)
+	mouse_input = Vector2.ZERO
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
