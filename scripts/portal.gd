0dfd4cb60a0ba7ed46dec2f1f5e844e4d149dad0
ldkuba <nawrocki97@gmail.com>
2025-01-25 16:10:56 +0100

    Add teleportation through portals

--- scripts/portal.gd
+++ scripts/portal.gd
@@ -5,6 +5,7 @@ class_name Portal
 @export var portal2: PortalDoor
 
 var player: Player
+var player_last_pos: Vector3 = Vector3.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -34,10 +35,23 @@ func _ready() -> void:
 	portal2.camera.environment = get_viewport().world_3d.environment.duplicate()
 	portal2.camera.environment.tonemap_mode = Environment.TONE_MAPPER_LINEAR
 
+func _crossed_portal(portal: PortalDoor, player_pos: Vector3, near_delta: float) -> bool:
+	var portal_a: Vector3 = portal.global_position + portal.global_transform.basis.x * 0.5 + player.camera.global_transform.basis.z * near_delta
+	var portal_b: Vector3 = portal.global_position - portal.global_transform.basis.x * 0.5 + player.camera.global_transform.basis.z * near_delta
+	var portal_a_xz: Vector2 = Vector2(portal_a.x, portal_a.z)
+	var portal_b_xz: Vector2 = Vector2(portal_b.x, portal_b.z)
+	var player_pos_xz: Vector2 = Vector2(player_pos.x, player_pos.z)
+	var player_last_pos_xz: Vector2 = Vector2(player_last_pos.x, player_last_pos.z)
+
+	var direction: int = sign((player_pos - player_last_pos).cross(portal_b - portal_a).y)
+
+	return Geometry2D.segment_intersects_segment(player_pos_xz, player_last_pos_xz, portal_a_xz, portal_b_xz) != null && direction == 1
+
 func _get_camera_transform(portal: PortalDoor, other_portal: PortalDoor, player_camera: Camera3D) -> Transform3D:
 	var playerCamToPortal: Transform3D =  portal.global_transform.affine_inverse() * player_camera.global_transform
-	var transform = other_portal.global_transform * playerCamToPortal.rotated(Vector3(0, 1, 0), PI)
-	return transform
+	var t: Transform3D = other_portal.global_transform * playerCamToPortal.rotated(Vector3(0, 1, 0), PI)
+	return t
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# Set transform of portal1 camera
@@ -47,3 +61,18 @@ func _process(delta: float) -> void:
 	# Set transform of portal2 camera
 	portal2.camera.global_transform = _get_camera_transform(portal1, portal2, player.camera)
 	portal2.viewport.size = get_viewport().get_visible_rect().size
+
+	# Handle teleporting player
+	var player_pos: Vector3 = player.global_position
+	# if player_last_pos != player_pos:
+	var near_delta: float = player.camera.near
+	if _crossed_portal(portal1, player_pos, near_delta):
+		var playerRelativePortal1: Transform3D = portal1.global_transform.affine_inverse() * player.global_transform
+		player.global_transform = portal2.global_transform * playerRelativePortal1.rotated(Vector3(0, 1, 0), PI)
+		player.global_position += portal2.global_transform.basis.z * near_delta
+	elif _crossed_portal(portal2, player_pos, near_delta):
+		var playerRelativePortal2: Transform3D = portal2.global_transform.affine_inverse() * player.global_transform
+		player.global_transform = portal1.global_transform * playerRelativePortal2.rotated(Vector3(0, 1, 0), PI)
+		player.global_position += portal1.global_transform.basis.z * near_delta
+
+	player_last_pos = player_pos
