86e15609f6e7a5e40ac6023a67fd01aee819d566
tracefree <riedev@proton.me>
2025-01-25 10:38:51 +0100

    He now can go around corners without getting stuck

--- scripts/monster.gd
+++ scripts/monster.gd
@@ -15,20 +15,18 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var next_path_position := agent.get_next_path_position()
 	var new_velocity := global_position.direction_to(next_path_position) * speed
-	agent.velocity = new_velocity
-	#_on_velocity_computed(new_velocity)
+	if agent.avoidance_enabled:
+		agent.velocity = new_velocity
+	else:
+		_on_velocity_computed(new_velocity)
 
 
 func _on_detection_body_entered(body: Node3D) -> void:
-	print("Detected ", body)
 	if body is Player:
 		state_machine.transition_to(&"Pursue", { &"target": body })
 
 
 func _on_velocity_computed(safe_velocity: Vector3) -> void:
-	print("Safe velocity: ", safe_velocity)
 	velocity = safe_velocity
-	sprite.look_at(Vector3(
-		safe_velocity.x, 0.0, safe_velocity.z
-	).normalized())
+	sprite.look_at(agent.target_position)
 	move_and_slide()
