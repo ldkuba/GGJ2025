3442d529884ed34d2e173795571c53226b8c36c8
hannes <hannes.rother@online.de>
2025-01-25 23:12:16 +0100

    map overlay WIP

---
+++ scripts/level.gd
@@ -0,0 +1,18 @@
+extends Node3D
+class_name Level
+
+@export var floor: VisualInstance3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func get_dimensions() -> AABB:
+	print(floor.get_path())
+	var aabb: AABB = floor.get_aabb()
+	return aabb
--- scripts/map.gd
+++ scripts/map.gd
@@ -1,17 +1,21 @@
 extends Sprite3D
 
+var level: Level
 @export var animation_player: AnimationPlayer
 @export var overlay: Sprite3D
 @export var player: Node3D
-@export var level_max_dimensions: Vector2 = Vector2(10, 10)
-@export var level_min_dimensions: Vector2 = Vector2(0, 0)
+var level_max_dimensions: Vector2 = Vector2(10, 10)
+var level_min_dimensions: Vector2 = Vector2(0, 0)
+var aabb: AABB
 @export var map_size: Vector2 = Vector2(1, 1)
 var map_visible := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	level = get_tree().root.find_child("Level", true, false) as Level
+	if not level:
+		printerr("No level found!")
+		return
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("toggle_map"):
@@ -23,8 +27,20 @@ func _input(event: InputEvent) -> void:
 			animation_player.play_backwards("toggle_map")
 
 func _process(delta: float) -> void:
+	#update dimensions
+	if not aabb:
+		aabb = level.get_dimensions()
+		print("level aabb: ", aabb , "end: ", aabb.end)
+		#aabb.position = level.global_position + aabb.position
+		aabb = level.transform * aabb
+		print("level aabb: ", aabb , "end: ", aabb.end)
+		level_max_dimensions = Vector2(aabb.end.x, aabb.position.z)
+		level_min_dimensions = Vector2(aabb.position.x, aabb.end.z)
+		print("level dimensions: ", level_max_dimensions, " ", level_min_dimensions)
+
 	if map_visible and player and overlay:
 		overlay.rotation.z = player.rotation.y
+		print("player position: ", player.position)
 		overlay.position.x = from_world_to_map(player.position.x, level_max_dimensions.x, level_min_dimensions.x, map_size.x)
 		overlay.position.y = from_world_to_map(player.position.z, level_max_dimensions.y, level_min_dimensions.y, map_size.y)
 
