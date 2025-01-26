f207d95e3050fe459d941d1f8ca3859124e7770c
ldkuba <nawrocki97@gmail.com>
2025-01-26 16:32:55 +0100

    Portal shader

--- scripts/portal_door.gd
+++ scripts/portal_door.gd
@@ -5,6 +5,7 @@ class_name PortalDoor
 @export var camera: Camera3D
 @export var viewport: SubViewport
 @export var spawn_collider: Area3D
+@export var light: OmniLight3D
 
 @export var border_color: Color = Color(0, 0, 0, 1)
 @export var border_size: float = 0.1
@@ -21,11 +22,14 @@ func set_portal_active(active: bool) -> void:
 	if _portal_active:
 		mesh.set_instance_shader_parameter("portal_active", 0)
 		RenderingServer.viewport_set_update_mode(viewport.get_viewport_rid(), 2) # When visible
+		light.visible = true
 	else:
 		mesh.set_instance_shader_parameter("portal_active", 1)
 		RenderingServer.viewport_set_update_mode(viewport.get_viewport_rid(), 0) # Disabled
+		light.visible = false
 
 func _ready() -> void:
+	light.light_color = border_color
 	set_portal_active(_portal_active)
 
 func is_colliding() -> bool:
